f0cb8fd17683e380b892f06c7e841495adafa4f0
Mendel Stillerman <menstiler@gmail.com>
2025-05-29 22:02:05 -0400

    added platform specific styling

--- components/ui/Title.js
+++ components/ui/Title.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet, Text, Platform } from "react-native";
 
 function Title({ children }) {
   return <Text style={styles.title}>{children}</Text>;
@@ -12,7 +12,7 @@ const styles = StyleSheet.create({
     fontSize: 24,
     color: "white",
     textAlign: "center",
-    borderWidth: 2,
+    borderWidth: Platform.select({ ios: 0, android: 2 }),
     borderColor: "white",
     padding: 12,
     maxWidth: "80%",
